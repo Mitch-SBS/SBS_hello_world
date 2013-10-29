f79d3198e9c649fea8287cb7f73a076e69f8e30f
Mitchel Frick <Mitch@smallblockstudios.com>
2013-10-29 10:21:26 -0400

    initializing repo

---
+++ gen/com/smallblockstudios/sbs_hello_world/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.smallblockstudios.sbs_hello_world;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
